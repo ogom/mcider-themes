e7600fb9b35635a5ef4d7a20ce2651e26dd5f365
ogom <ogom@hotmail.co.jp>
2014-12-23 18:00:55 +0900

    Initial commit

---
+++ io2012/js/slides.js
@@ -0,0 +1,11 @@
+/**
+ * ogom modifications
+ *   Fixed slide_config path.
+ *   Added slide-resize.
+ */
+
+require(['order!slide_config', 'order!modernizr.custom.45394',
+         'order!prettify/prettify', 'order!hammer', 'order!slide-controller',
+         'order!slide-deck', 'order!slide-resize'], function(someModule) {
+
+});
